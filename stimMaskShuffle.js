1834e8a887c278d5bd642720795f206b69f61400
J-P <jon-paul.cavallaro@uon.edu.au>
2020-04-17 15:45:56 +1000

    Preliminary test Exp - check timing/responses

--- stimMaskShuffle.js
+++ stimMaskShuffle.js
@@ -7,53 +7,20 @@
 var features = [												// Available phone features and the random values it can have
   ["Low Price", "High Price",],
   ["Low Quality", "High Quality",],
-   ];
-
-
-   console.log(features[1][0])
+   ]; console.log(features[1][0])
 let numPhones = 2
-
 let mask = ['L','o','w','P','r','i','c','e',
 'H','i','g','h','P',"r",'i','c','e','L',
 'o','w','Q','u','a','l','i','t','y','H',
 'i','g','h','Q','u','a','l','i','t','y',]
-
 let maskLength = 12
 let usingFeatures = [1,2]
 let maskArray = []
 let numFeatures = 2
-//let attributeMask = []
-/*
-function createPhoneMask() {
-  let optionMask = {masks:[]}
-  for (j = 0; j < numFeatures; j++ ){
-// this function will be same as createNewPhone
-    for (i = 0; i < maskLength; i++) {
-    // Pick a random value for the feature
-    var maskElement = Math.floor(Math.random() * mask.length);
-    console.log(maskElement)
-    optionMask += mask[maskElement]
-    console.log(optionMask)
-  }
-  optionMask.masks[usingFeatures[i]] = 
-}
-return optionMask
-}
 
-let numMasks = 4
-*/
-
-function createMaskedPhone (){
-  let maskPhone = {atts:[]}
-  for(j = 0; j < numFeatures; j++ ) {    
-    maskPhone.atts.push(createAttributeMask())
-  }
-return maskPhone
-}
-
-function createAttributeMask () {
-  let attributeMask = []
-    for (i = 0; i < maskLength; i++) {
+function createAttributeMask () { // Create single attribute mask
+  var attributeMask = []
+    for (var i = 0; i < maskLength; i++) {
       // Generate random number from 0 - number of letters in mask array
       var maskElement = Math.floor(Math.random() * mask.length);
       // concatenate letters in 12 character string i.e. mask
@@ -62,91 +29,18 @@ function createAttributeMask () {
     return attributeMask
   }
 
-console.log(createMaskedPhone())
-
-
-
-
-
-
-
-function createNewPhone() {								//creates phones
-  //console.log("createNewPhone");
-  // New Phone
-  var phone = {features:[]};
-  // Add the features
-  for (var i = 0; i < numFeatures; i++) {
-      // Pick a random value for the feature
-      var randomValue = Math.floor(Math.random() * (features[usingFeatures[i]].length));
-      phone.features[usingFeatures[i]] = features[usingFeatures[i]][randomValue];
-      // 
+function createMaskedPhone () { // Create array containing mask for single option i.e. two attributes
+  var maskPhone = {atts:[]}
+  for(var j = 0; j < numFeatures; j++ ) {    
+    maskPhone.atts.push(createAttributeMask())
   }
-  // Return the new phone
-  return phone;
+return maskPhone
 }
 
-
-
- 
-
-// Push feature masks into maskArray object
-for (var i = 0; i < numPhones; i++) {
-  maskArray.push(createAttributeMask());
+for(var k = 0; k < numFeatures; k++){
+maskArray.push(createMaskedPhone())
 }
-
 console.log(maskArray)
 
 
-/*if(feature == "Low Price")
-optionMask == ["3 5"]
-*/
 
-/*
-
-let featureMask = mask[1] + mask[17] + mask[4] + mask[5] + mask[9] + mask[20] + mask[28] + mask[13] +
-mask[17] + mask[10] + mask[17] + mask[29]
-
-console.log(mask.length)
-console.log(featureMask)
-
-
-function shuffleArr(arr) {    // This shuffle/randomise function lifted from https://bost.ocks.org/mike/shuffle/
-                              // Shuffle function - Pick a random element, swap with last element in back of array/list.
-  let m = arr.length, t, i;   // Shorthand for var m = arr.length, var t;, var i; i.e. multiple elements  
-    while (m) {               // While there remain elements to shuffle (i.e. m > 0)
-                              // Pick a remaining element
-                              // Generate a random number within the range of no. durations array and round down
-      i = Math.floor(Math.random() * m--); 
-                              // And swap it with the current element.
-      t = arr[m];
-                              // var t = the last element in durations array/list
-      arr[m] = arr[i];
-                              // Swap the last element in the durations array/list to the
-                              // randomly chosen element in the durations array/list
-      arr[i] = t;
-                              // and swap element durations[i] with the formerly last element of the durations array/list
-    }
-  return arr;
-  //console.log(shuffleArr)
-}
-
-
-
-/*console.log(shuffleArr(mask)) //
-
-
-var word = "4f563a46jy7u35616";
-
-function mak() {
-  if (word.length <=4) {
-    return word
-  } else {
-    var masked = word.substring(0, word.length - 4).replace(/[a-z\d]/gi,"uLyiatlity") + 
-    word.substring(word.length - 4, word.length)
-    return masked;
-  }
-}
-
-console.log(mak(word))
-
-*/
